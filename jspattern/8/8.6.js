31ef7a64cdc16befd46adf05a19aa8ab3efcaccb
nao-web <info@nao-web.com>
2013-02-18 14:53:37 +0900

    8.6

---
+++ jspattern/8/8.6.js
@@ -0,0 +1,11 @@
+// 8.6.1
+// command line tools
+$ cat jquery.js jquery.quickselect.js jquery.limit.js > all.js
+
+// 8.6.2
+AddOutputFilterByType DEFLATE text/html text/css text/plain text/xml
+application/javascript application/json
+
+// 8.6.3
+ExpiresActive On
+ExpiresByType application/x-javascript "access plus 10 years"
